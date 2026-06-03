bcdf823c0c54a9e09dff03f2cd4894de6a67b706
Jon Earle <jonearle05@gmail.com>
2026-06-03 10:34:57 -0300

    Updated receive_position.py for current CSV

--- script/maps/map.py
+++ script/maps/map.py
@@ -5,7 +5,7 @@ map = folium.Map(location=[44.6488, -63.5930], zoom_start=11)
 
 routeCoordinates = []
 
-with open("/home/jonearle/usra2026/data/bike_tests/bike_nearby2_long_fast.csv", mode='r') as file:
+with open("/Users/Jon/USRA2026/data/bike_tests/bike_north_long_fast_fail.csv", mode='r') as file:
     reader = csv.DictReader(file)
     next(reader)
     for row in reader:
@@ -16,4 +16,4 @@ with open("/home/jonearle/usra2026/data/bike_tests/bike_nearby2_long_fast.csv", 
 
 folium.PolyLine(routeCoordinates).add_to(map)
     
-map.save("/home/jonearle/usra2026/data/routes/nearby_long_fast_2.html")
+map.save("/Users/Jon/USRA2026/data/routes/desk.html")
